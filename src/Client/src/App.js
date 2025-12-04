96b0570187244918c7a4f43384a018c712de54ac
Suvini Mayadunna <suvini@example.com>
2025-12-04 23:22:22 +0530

    Enhance HomePage layout and styling; update routing in App.js

--- src/Client/src/App.js
+++ src/Client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 import SalesOrderPage from './pages/SalesOrderPage';
 import './styles/App.css';
 
@@ -8,7 +9,8 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<SalesOrderPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/orders" element={<SalesOrderPage />} />
         </Routes>
       </div>
     </Router>
